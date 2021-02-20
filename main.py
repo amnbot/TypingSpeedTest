f8a515197a22c246025dbfddf76434518f642aa0
spaceAlDS <57155052+spaceAlDS@users.noreply.github.com>
2021-02-20 17:10:24 -0500

    leaderboard

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 from second import second
 import json
 import random
+import os
 
 app = Flask(__name__, template_folder='templates')
 app.register_blueprint(second)
@@ -10,12 +11,7 @@ app.register_blueprint(second)
 def home():
     return render_template('index.html', prompt=get_prompt())
 
-@app.route('/', methods=['POST'])
-def home_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    
-    return processed_text
+
 
 def get_prompt():
     with open("db.json") as prompts:
@@ -24,6 +20,39 @@ def get_prompt():
             #return random.choice(data['prompts'])
 
             return make_sentence()
+
+@app.route('/submit-score', methods=['POST'])
+def submit_score():
+    user = {
+        "name": request.form["name"],
+        "time": float(request.form["time"])
+    }
+    data = None
+    
+    with open("leaderboard.json") as leaderboard:
+        data = json.load(leaderboard)
+        
+        already_exists = False
+        for _user in data["users"]:
+            if _user["name"] == user["name"]:
+                _user["time"] = user["time"]
+                already_exists = True
+                break
+        
+        if not already_exists:
+            data["users"].append(user)
+        
+    with open("leaderboard.json", "w") as leaderboard:
+        if data:
+            json.dump(data, leaderboard, indent=True)
+    
+    return request.form
+
+@app.route('/jquery-3.5.1.js', methods=['GET'])
+def jquery():
+    return send_file(os.getcwd() + "/jquery-3.5.1.js")
+
+
 def make_sentence():
     sentence = ""
     
